5c1cdcc42b3f8f7ac45feed994e70863e76c32f5
xbgood <iamxiebiao@outlook.com>
2022-05-05 23:57:12 +0800

    indent, 4 space

--- lua/basic/keybinds.lua
+++ lua/basic/keybinds.lua
@@ -1,6 +1,6 @@
 -- leader 键设置为空格                                    
 vim.g.mapleader = ","
-    
+
 -- 默认的键位设置函数太长了，所以这里将它们重新引用一下
 vim.keybinds = {
     gmap = vim.api.nvim_set_keymap,
@@ -14,10 +14,10 @@ vim.keybinds = {
 vim.keybinds.gmap("i", "jj", "<Esc>", vim.keybinds.opts)
 
 -- 用 H 和 L 代替 ^ 与 $
-vim.keybinds.gmap("n", "H", "^", vim.keybinds.opts)
-vim.keybinds.gmap("v", "H", "^", vim.keybinds.opts)
-vim.keybinds.gmap("n", "L", "$", vim.keybinds.opts)
-vim.keybinds.gmap("v", "L", "$", vim.keybinds.opts)
+-- vim.keybinds.gmap("n", "H", "^", vim.keybinds.opts)
+-- vim.keybinds.gmap("v", "H", "^", vim.keybinds.opts)
+-- vim.keybinds.gmap("n", "L", "$", vim.keybinds.opts)
+-- vim.keybinds.gmap("v", "L", "$", vim.keybinds.opts)
 
 -- 将 C-u 和 C-d 调整为上下滑动 10 行而不是半页
 vim.keybinds.gmap("n", "<C-u>", "10k", vim.keybinds.opts)
