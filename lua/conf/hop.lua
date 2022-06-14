a4d4aa3d0a28f93d1d407f32fd77360b06e88fbe
xbgood <iamxiebiao@outlook.com>
2022-06-14 16:43:48 +0800

    add hop.lua search char end

--- lua/conf/hop.lua
+++ lua/conf/hop.lua
@@ -1,6 +1,9 @@
 -- https://github.com/phaazon/hop.nvim
 
-require("hop").setup()
+require("hop").setup{
+    keys = 'etovxqpdygfblzhckisuran',
+    jump_on_sole_occurrence = false
+}
 
 -- 搜索并跳转到单词
 vim.keybinds.gmap("n", "<leader>hw", "<cmd>HopWord<CR>", vim.keybinds.opts)
@@ -8,3 +11,9 @@ vim.keybinds.gmap("n", "<leader>hw", "<cmd>HopWord<CR>", vim.keybinds.opts)
 vim.keybinds.gmap("n", "<leader>hl", "<cmd>HopLine<CR>", vim.keybinds.opts)
 -- 搜索并跳转到字符
 vim.keybinds.gmap("n", "<leader>hc", "<cmd>HopChar1<CR>", vim.keybinds.opts)
+
+
+-- place this in one of your configuration file(s)
+vim.api.nvim_set_keymap('n', '<leader>he', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", {})
+vim.api.nvim_set_keymap('v', '<leader>he', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END })<cr>", {})
+vim.api.nvim_set_keymap('o', '<leader>he', "<cmd> lua require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, inclusive_jump = true })<cr>", {})
